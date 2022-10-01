f3b2c316550950fa17f1aac3f446818b770a26cd
Nazareno Soto <nazitasoto99@gmail.com>
2022-10-01 00:55:18 -0300

    Solucione el error al subir los productos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-3//Loader (Esto lo investigué en un lugar externo al curso)
+//Loader (Esto lo investigué en un lugar externo al curso)
 window.addEventListener('load', () => {
     const contenedor_loader = document.querySelector ('.contenedor_loader')
     contenedor_loader.style.opacity = 0
     contenedor_loader.style.visibility = 'hidden'
     console.log("La pagina ha iniciado")
 })
+//Subir productos
 //Subir productos
 const product = []
 let idSolit = 0
@@ -30,18 +31,19 @@ function crearProducto() {
     const Producto = document.createElement('div')
     product.forEach( (x) => {
         Producto.innerHTML = `
-        <div class="row productoss">
-        <div class="producto col-md-4 col-lg-3 col-sm-1">
-        <img src="./img/cards/Ropa-del-usuario.jpg" alt="${x.Producto}" class="imagenes">
-        <div class="informacion">
-            <span class="tipo-envio">Envio con normalidad</span>
-            <span class="costo-envio">Envio gratis</span>
-            <span class="prenda">${x.Producto}</span>
-            <a href="#" class="aa">Contactar</a>
-            <a href="#" class="carrito compra" id="envento">Agregar al carrito de compras</a>
+        <div class="roww">
+                <div class="four colummns">
+                    <div class="card">
+                        <img src="../img/cards/Ropa-del-usuario.jpg" class="imagen-curso u-full-width">
+                        <div class="info-card">
+                            <h4>${x.Producto}</h4>
+                            <p>Argentina</p>
+                            <img src="http://programacion.net/files/article/20160811120805_estrellas.png" width="50">
+                            <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="1">Agregar Al Carrito</a>
+                        </div>
+                    </div> <!--.card-->
+                </div>
         </div>
-    </div>
-    </div>
         `
     });
     listProduc.appendChild(Producto)
